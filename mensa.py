fd0058175c292ad08b49c240328103a996d0d3c4
Tim Weissker <me@tim-weissker.de>
2026-05-28 20:22:21 +0200

    Initial commit featuring mensa script.

---
+++ mensa.py
@@ -0,0 +1,305 @@
+#!/bin/python
+import argparse
+import datetime
+import requests
+import bs4
+import os
+import sys
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+mensa_url = 'https://www.studierendenwerk-aachen.de/speiseplaene/$(MENSA_NAME)-w$(LANG_MODIFIER).html'
+lang_modifiers = {'en': '-en', 'de': ''}
+
+mensa_names = {'vita': ('vita', 'Mensa Vita'),
+               'acad': ('academica', 'Mensa Academica'),
+               'ahor': ('ahornstrasse', 'Mensa Ahornstraße'),
+               'temp': ('templergraben', 'Bistro Templergraben'),
+               'baye': ('bayernallee', 'Mensa Bayernallee'),
+               'kmac': ('kmac', 'Mensa KMAC'),
+               'eupe': ('eupenerstrasse', 'Mensa Eupener Straße'),
+               'sued': ('suedpark', 'Mensa Südpark'),
+               'juel': ('juelich', 'Mensa Jülich')}
+
+empty_message = {'en': 'No menu is offered today',
+                 'de': 'Heute gibt es kein Angebot'}
+
+weekdays = {'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            'de': ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']}
+
+date_format = '%d.%m.%Y'
+
+today = datetime.datetime.today().replace(
+        hour=0, minute=0, second=0, microsecond=0)
+
+
+class MenuItem:
+    item_type = ""
+    description = ""
+    price = ""
+    vegetarian = False
+    vegan = False
+
+    def __init__(self, item_type, description, price, vegetarian=False, vegan=False):
+        self.item_type = item_type
+        self.description = description
+        self.price = price
+        self.vegetarian = vegetarian
+        self.vegan = vegan
+
+    def __str__(self, compact=False):
+        leader = ''
+        if compact:
+            leader = '- '
+
+        if compact:
+            return f'{leader}{self.description} | {self.price}\n'
+        else:
+            return f'{self.item_type}\n{leader}{self.description}\n{self.price}\n\n'
+
+
+class DayMenu:
+    date = datetime.datetime.today()
+    language = 'en'
+    menu_items = []
+
+    def __init__(self, date, language):
+        self.date = date
+        self.language = language
+        self.menu_items = []
+
+    def __str__(self, vegetarian=False, vegan=False, compact=False):
+        final_string = f'{weekdays[self.language][self.date.weekday()]}, {self.date.strftime(date_format)}\n'
+        final_string += (len(final_string)+1) * '-' + '\n'
+
+        for menu_item in self.menu_items:
+            if not (vegetarian and menu_item.vegetarian == False) and \
+               not (vegan and menu_item.vegan == False):
+                final_string += menu_item.__str__(compact)
+
+        return final_string
+
+
+def main():
+    arguments = parse_command_arguments()
+
+    get_url = mensa_url.replace(
+        '$(MENSA_NAME)', mensa_names[arguments.mensa][0])
+    get_url = get_url.replace(
+        '$(LANG_MODIFIER)', lang_modifiers[arguments.lang])
+
+    menu_list = get_all_menus(get_url, arguments.lang)
+
+    nutrition_string = ''
+    if arguments.vegan:
+        nutrition_string = ' (Vegan Meals Only)'
+    elif arguments.vegetarian:
+        nutrition_string = ' (Vegetarian Meals Only)'
+    print_headline(
+        f'Menu for {mensa_names[arguments.mensa][1]}{nutrition_string}')
+
+    relative_list = get_relative_list(menu_list)
+    print_list = get_print_list(
+        relative_list, arguments.num_past, arguments.num_future)
+
+    if print_list.count(True) > 0:
+        print_relevant_menus(
+            menu_list, print_list, arguments.vegetarian, arguments.vegan, arguments.long)
+        if arguments.screenshot and print_list.count(True) > 0:
+            screenshot(get_url, menu_list, relative_list, print_list,
+                       'screenshots/', arguments.mensa)
+
+    else:
+        print(empty_message[arguments.lang])
+
+
+def parse_command_arguments():
+    list_of_mensas = 'Available locations are '
+    for key in mensa_names.keys():
+        list_of_mensas += f'{mensa_names[key][1]} ({key}), '
+    list_of_mensas = list_of_mensas[:-2]
+
+    parser = argparse.ArgumentParser(
+        prog='mensa.py',
+        description="Display what's on the menu at one of Aachen's finest dining places",
+        epilog=list_of_mensas)
+    parser.add_argument('-m', '--mensa', help="the mensa to retrieve the menu for, default is 'vita'",
+                        choices=mensa_names.keys(), default=list(mensa_names.keys())[0])
+    parser.add_argument('-p', '--past', type=int, help="print previous NUM_PAST menus, default is all",
+                        nargs='?', const=20, default=0, dest='num_past')
+    parser.add_argument('-f', '--future', type=int, help="print next NUM_FUTURE menus, default is all",
+                        nargs='?', const=20, default=0, dest='num_future')
+    parser.add_argument('-v', '--vegetarian', action='store_true',
+                        help="only show vegetarian options")
+    parser.add_argument('-vv', '--vegan', action='store_true',
+                        help="only show vegan options")
+    parser.add_argument('-l', '--long', action='store_true',
+                        help="use long instead of compact output, including dish category")
+    parser.add_argument('-lg', '--lang', help="select the language to retrieve, default is 'en'",
+                        choices=['en', 'de'], default='en')
+    parser.add_argument('-s', '--screenshot', action='store_true',
+                        help="save a screenshot of each selected menu")
+    return parser.parse_args()
+
+
+def get_all_menus(url, lang_shorthand):
+    soup = download_current_menu_data(url)
+
+    menu_root_nodes = soup.find_all('div', 'preventBreak')
+    menus = []
+
+    for menu_root in menu_root_nodes:
+        day_menu = get_day_menu(menu_root, lang_shorthand)
+        menus.append(day_menu)
+
+    return menus
+
+
+def download_current_menu_data(url):
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
+
+    if response.status_code == 200:
+        return bs4.BeautifulSoup(response.text, 'html.parser')
+
+    else:
+        print(
+            f'Error retrieving page. Response code is: {response.status_code}')
+        sys.exit(1)
+
+
+def get_day_menu(menu_root_node, language):
+    date_string = get_text_or_default(menu_root_node.find('a'))
+    date_string = date_string.split(',')[1].lstrip()  # remove weekday
+    datetime_object = datetime.datetime.strptime(date_string, date_format)
+    day_menu = DayMenu(datetime_object, language)
+
+    menu_table = menu_root_node.find('table', 'menues')
+
+    for menu_item_row in menu_table.find_all('tr'):
+        item_type = get_text_or_default(
+            menu_item_row.find('span', 'menue-category'))
+        item_description = get_text_or_default(
+            menu_item_row.find('span', 'expand-nutr'))
+        item_price = get_text_or_default(menu_item_row.find(
+            'span', 'menue-price'), default='Unknown Price')
+        item_nutrition = get_nutrition(menu_item_row)
+        menu_item = MenuItem(item_type, item_description,
+                             item_price, item_nutrition[0], item_nutrition[1])
+        day_menu.menu_items.append(menu_item)
+
+    return day_menu
+
+
+def get_text_or_default(html, default=""):
+    return_string = ""
+
+    try:
+        # remove letters in <sup>-tag (allergene letters)
+        copy_html = html
+        for tag in copy_html.find_all('sup'):
+            tag.replace_with('')
+
+        return_string = copy_html.get_text()
+
+        # clean input string for undesired characters
+        return_string = return_string.replace('\n', '')
+        return_string = return_string.replace('+', '')
+
+        # remove duplicate spaces
+        return_string = ' '.join(return_string.split())
+    except:
+        return_string = default
+
+    return return_string
+
+
+def get_nutrition(html):
+    is_vegetarian = 'OLV' in html['class']
+    is_vegan = 'vegan' in html['class']
+    return (is_vegetarian, is_vegan)
+
+
+def get_relative_list(menu_list):
+    relative_list = list(map(lambda menu: (menu.date-today).days, menu_list))
+    relative_list = list(map(lambda id: sign(id), relative_list))
+    past_days = relative_list.count(-1)
+    future_days = relative_list.count(1)
+
+    for i in range(past_days):
+        relative_list[i] = -past_days
+        past_days = past_days - 1
+
+    for j in range(future_days):
+        relative_list[-(j+1)] = future_days
+        future_days = future_days - 1
+
+    return relative_list
+
+
+def sign(x):
+    if x < 0:
+        return -1
+    elif x > 0:
+        return 1
+    else:
+        return 0
+
+
+def get_print_list(relative_list, num_past, num_future):
+    print_list = [False for i in range(len(relative_list))]
+
+    for i in range(len(relative_list)):
+        if (relative_list[i] < 0 and relative_list[i] >= -num_past) or \
+           (relative_list[i] > 0 and relative_list[i] <= num_future) or \
+           (relative_list[i] == 0):
+
+            print_list[i] = True
+
+    return print_list
+
+
+def print_relevant_menus(menu_list, print_list, vegetarian, vegan, long_output):
+    print_count = 0
+
+    for i in range(len(print_list)):
+        if print_list[i]:
+            print(menu_list[i].__str__(vegetarian=vegetarian,
+                  vegan=vegan, compact=not long_output))
+            print_count += 1
+
+
+def screenshot(url, menu_list, relative_list, print_list, output_dir, filename_prefix):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless=new")
+    browser = webdriver.Firefox(options=options)
+    browser.get(url)
+    menu_accordion_items = browser.find_elements(
+        By.CSS_SELECTOR, '.preventBreak')
+
+    for i in range(len(print_list)):
+        if print_list[i]:
+
+            if relative_list[i] != 0: # only click if not today because it is open already otherwise
+                menu_accordion_items[i].click()
+                time.sleep(1)
+            file_name = f'{filename_prefix}-{menu_list[i].date.strftime("%Y-%m-%d")}'
+            menu_accordion_items[i].screenshot(f'{output_dir}/{file_name}.png')
+
+    browser.close()
+
+
+def print_headline(text):
+    print((len(text) + 4) * '#')
+    print('# ' + text + ' #')
+    print((len(text) + 4) * '#')
+    print()
+
+
+if __name__ == '__main__':
+    main()
